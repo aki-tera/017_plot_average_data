671fa7712dc457f38882988b72e8f905b5bfde84
aki-tera <aki.tera@gmail.com>
2020-06-27 23:34:40 +0900

    test

--- 017_plot_average_data.py
+++ 017_plot_average_data.py
@@ -4,89 +4,111 @@ import statistics
 from matplotlib.pyplot import figure
 import matplotlib.pyplot as plt
 
-path = "test.csv"
-
-check_code = re.compile("[0-9]{4}/[0-9]{2}/[0-9]{2}")
-
-data_a = np.arange(0)
-data_b = np.arange(0)
-data_c = np.arange(0)
-counter = 0
-
-data_start = ""
-data_end = ""
-
-# 中央値の取得
-z1_mid = []
-z2_mid = []
-
-with open(path) as f:
-    for temp_line in f:
-        # 日付のあるデータのみ抽出する
-        if re.match(check_code, temp_line) is not None:
-            temp_z1 = float(temp_line.split(",")[2])
-            temp_z2 = float(temp_line.split(",")[3])
-
-            # Z1の中央値を取得する
-            if temp_z1 < 5 and z1_mid == []:
-                z1_mid = [temp_z1]
-            elif temp_z1 < 5 and z1_mid != []:
-                z1_mid.append(temp_z1)
-            elif temp_z1 > 5 and z1_mid != []:
-                # Z1データの中央値をnumpy配列の代入
-                data_a = np.append(data_a, statistics.median(z1_mid))
-                z1_mid = []
-
-            # Z2の中央値を取得する
-            if temp_z2 < 5 and z2_mid == []:
-                z2_mid = [temp_z2]
-            elif temp_z2 < 5 and z2_mid != []:
-                z2_mid.append(temp_z2)
-            elif temp_z2 > 5 and z2_mid != []:
-                # Z1データの中央値をnumpy配列の代入
-                data_b = np.append(data_b, statistics.median(z2_mid))
-                z2_mid = []
-
-            # 開始時間を取得
-            if data_start == "":
-                data_start = temp_line.split(",")[0]
-            # 終了時間を取得
-            data_end = temp_line.split(",")[0]
-
-data_ax = np.arange(1, data_a.shape[0]+1)
-data_bx = np.arange(1, data_b.shape[0]+1)
+import glob
 
 
-print(data_a.shape[0])
-print(data_b.shape[0])
+class Nr600DataExtractor:
+    def __init__(self, filename):
+        # ファイル名
+        self.filename = filename
+        # データ取得用
+        self.data_a = np.arange(0)
+        self.data_b = np.arange(0)
+        self.data_c = np.arange(0)
+        # データの日付取得用
+        self.data_start = ""
+        self.data_end = ""
+
+    def median_data(self):
+        # 日付のチェック用
+        check_code = re.compile("[0-9]{4}/[0-9]{2}/[0-9]{2}")
+        # 中央値の取得
+        z1_mid = []
+        z2_mid = []
+        # ファイルからデータを取得する
+        with open(self.filename) as f:
+            for temp_line in f:
+                # 日付のあるデータのみ抽出する
+                if re.match(check_code, temp_line) is not None:
+                    temp_z1 = float(temp_line.split(",")[2])
+                    temp_z2 = float(temp_line.split(",")[3])
+                    # Z1の中央値を取得する
+                    if temp_z1 < 5 and z1_mid == []:
+                        z1_mid = [temp_z1]
+                    elif temp_z1 < 5 and z1_mid != []:
+                        z1_mid.append(temp_z1)
+                    elif temp_z1 > 5 and z1_mid != []:
+                        # Z1データの中央値をnumpy配列の代入
+                        self.data_a = np.append(self.data_a, statistics.median(z1_mid))
+                        z1_mid = []
+                    # Z2の中央値を取得する
+                    if temp_z2 < 5 and z2_mid == []:
+                        z2_mid = [temp_z2]
+                    elif temp_z2 < 5 and z2_mid != []:
+                        z2_mid.append(temp_z2)
+                    elif temp_z2 > 5 and z2_mid != []:
+                        # Z1データの中央値をnumpy配列の代入
+                        self.data_b = np.append(self.data_b, statistics.median(z2_mid))
+                        z2_mid = []
+                    # 開始時間を取得
+                    if self.data_start == "":
+                        self.data_start = temp_line.split(",")[0]
+                    # 終了時間を取得
+                    self.data_end = temp_line.split(",")[0]
+        # データ数を取得する
+        temp_ax = self.data_a.shape[0]+1
+        temp_bx = self.data_b.shape[0]+1
+        self.data_ax = np.arange(1, temp_ax)
+        self.data_bx = np.arange(1, temp_bx)
+        # 両者のデータが同じかどうかを確認する
+        if temp_ax == temp_bx:
+            return(True)
+        else:
+            return(False)
+
+    def formatter_data(self):
+        temp_filename = "resutl_"+self.filename
+        np.savetxt(temp_filename, np.array([self.data_a, self.data_b]), fmt="%.4f", delimiter=",")
+
+    def plot_data(self):
+        # Figureオブジェクトを作成
+        # ウインドウサイズを指定(横×縦)
+        fig = figure(figsize=(8, 4))
+        # 描画タイトルを表示
+        fig.suptitle(self.data_start+" -> "+self.data_end, fontweight="bold")
+        # figに属するAxesオブジェクトを作成
+        ax = fig.add_subplot(1, 1, 1)
+        # 折れ線グラフをプロット
+        ax.plot(self.data_ax, self.data_a)
+        ax.plot(self.data_bx, self.data_b)
+        # 凡例を表示
+        ax.legend(["Z1", "Z2"], loc="best", fontsize=8)
+        # grid表示ON
+        ax.grid(True)
+        # x範囲
+        ax.set_xlim(left=1)
+        # y範囲
+        ax.set_ylim(bottom=-1, top=2)
+        # X軸のラベル
+        ax.set_xlabel("count number")
+        # Y軸のラベル
+        ax.set_ylabel("[mm]")
 
 
-np.savetxt("sample.csv", np.array([data_a, data_b]), fmt="%.4f", delimiter=",")
+def main():
+    filename = glob.glob("*.csv")
+    for row in filename:
+        if row[:7] == "resutl_":
+            pass
+        else:
+            d = Nr600DataExtractor(row)
+            if d.median_data() is True:
+                d.formatter_data()
+            else:
+                print("両者のデータが異なるでファイル保存は行いません")
+            d.plot_data()
 
-# Figureオブジェクトを作成
-# ウインドウサイズを指定(横×縦)
-fig = figure(figsize=(8, 4))
-# 描画タイトルを表示
-fig.suptitle(data_start+" -> "+data_end, fontweight="bold")
+    plt.show()
 
-# figに属するAxesオブジェクトを作成
-ax = fig.add_subplot(1, 1, 1)
-
-
-ax.plot(data_ax, data_a)
-ax.plot(data_bx, data_b)
-
-# 凡例を表示
-ax.legend(["Z1", "Z2"], loc="best", fontsize=8)
-# grid表示ON
-ax.grid(True)
-# x範囲
-ax.set_xlim(left=1)
-# y範囲
-ax.set_ylim(bottom=-1, top=2)
-# X軸のラベル
-ax.set_xlabel("count number")
-# Y軸のラベル
-ax.set_ylabel("[mm]")
-# 表示指示
-plt.show()
+if __name__ == "__main__":
+    main()
